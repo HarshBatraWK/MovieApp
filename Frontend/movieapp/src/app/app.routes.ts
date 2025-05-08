a63a61f68a6c872e7a5a868bf3d7aac5027816ec
Lokesh-1111 <luckylokeshsharma1111@gmail.com>
2025-05-09 00:55:54 +0530

    admin

--- Frontend/movieapp/src/app/app.routes.ts
+++ Frontend/movieapp/src/app/app.routes.ts
@@ -4,13 +4,32 @@ import { AboutComponent } from './pages/about/about.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { MovieComponent } from './pages/movie/movie.component';
-import { AuthGuard } from './guards/auth.guard';
-import { NoAuthGuard } from './guards/no-auth.guard';
+import { AdminComponent } from './pages/admin/admin.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { AddMoviesComponent } from './pages/add-movies/add-movies.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-    { path: 'about', component: AboutComponent, canActivate: [AuthGuard] },
-    { path: 'login', component: LoginComponent, canActivate: [NoAuthGuard] },
-    { path: 'signup', component: SignupComponent, canActivate: [NoAuthGuard] },
-    { path: 'movie/:id', component: MovieComponent, canActivate: [AuthGuard] }
+
+    {
+        path: '',
+        component: MainLayoutComponent,
+        children: [
+            { path: '', component: HomeComponent },
+            { path: 'about', component: AboutComponent },
+            { path: 'login', component: LoginComponent },
+            { path: 'signup', component: SignupComponent },
+            { path: 'movie/:id', component: MovieComponent }
+        ]
+      },
+      {
+        path: 'admin',
+        component: AdminLayoutComponent,
+        children: [
+            { path: '', component:AdminComponent },
+            { path: 'addmovie',component:AddMoviesComponent }
+        ]
+      },
+      { path: '**', component:NotFoundComponent }
 ];
